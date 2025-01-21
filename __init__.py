6a5f5068c5b38d9731f10e08cd189146cec7ebaf
xmarked-ai <marat@xmarked.com>
2025-01-21 12:48:50 +0300

    add LoraBatchX

--- __init__.py
+++ __init__.py
@@ -1,30 +1,10 @@
-# import ssl
-# ssl._create_default_https_context = ssl._create_unverified_context
-
-# # or
-
-# import requests
-# from huggingface_hub import configure_http_backend
-
-# def backend_factory() -> requests.Session:
-#     session = requests.Session()
-#     session.verify = False
-#     return session
-
-# configure_http_backend(backend_factory=backend_factory)
-
-# from .ColorGrade         import GRADE_CLASS_MAPPINGS, GRADE_NAME_MAPPINGS
-# from .RelightX           import RELIGHT_CLASS_MAPPINGS, RELIGHT_NAME_MAPPINGS
-# from .TileImageSquare    import TILE_CLASS_MAPPINGS, TILE_NAME_MAPPINGS
-# from .DepthDisplace      import DEPTHDISPLACE_CLASS_MAPPINGS, DEPTHDISPLACE_NAME_MAPPINGS
-# from .NF4_bnb_loaderX    import NF4BNB_CLASS_MAPPINGS, NF4BNB_NAME_MAPPINGS
-
 from .ColorGrade         import GRADE_CLASS_MAPPINGS, GRADE_NAME_MAPPINGS
 from .RelightX           import RELIGHT_CLASS_MAPPINGS, RELIGHT_NAME_MAPPINGS
 from .TileImageSquare    import TILE_CLASS_MAPPINGS, TILE_NAME_MAPPINGS
 from .DepthDisplace      import DEPTHDISPLACE_CLASS_MAPPINGS, DEPTHDISPLACE_NAME_MAPPINGS
 from .NF4_bnb_loaderX    import NF4BNB_CLASS_MAPPINGS, NF4BNB_NAME_MAPPINGS
 from .AceColorFixX       import ACECOLORFIXX_CLASS_MAPPINGS, ACECOLORFIXX_NAME_MAPPINGS
+from .LoraBatchX         import LORAX_CLASS_MAPPINGS, LORAX_NAME_MAPPINGS
 
 WEB_DIRECTORY = "./js"
 
@@ -48,5 +28,8 @@ NODE_DISPLAY_NAME_MAPPINGS.update(NF4BNB_NAME_MAPPINGS)
 
 NODE_CLASS_MAPPINGS.update(ACECOLORFIXX_CLASS_MAPPINGS)
 NODE_DISPLAY_NAME_MAPPINGS.update(ACECOLORFIXX_NAME_MAPPINGS)
+
+NODE_CLASS_MAPPINGS.update(LORAX_CLASS_MAPPINGS)
+NODE_DISPLAY_NAME_MAPPINGS.update(LORAX_NAME_MAPPINGS)
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', "WEB_DIRECTORY"]
